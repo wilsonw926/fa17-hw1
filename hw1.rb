d7e0410c0e2729b1d104143cd7f63be7e1abfc48
Wilson Wang <wilsonw926@gmail.com>
2017-09-15 09:54:27 -0700

    finished q1,q2,q3

--- hw1.rb
+++ hw1.rb
@@ -1,18 +1,18 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
-  sum = a + b
-  return sum ** 2
+  return (a + b) ** 2
 
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-
+  sort = a.sort
+  return sort.map {|x| x+1}
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-
+  return "#{first_name} #{last_name}"
 end
 
 def blockin_time(a)
