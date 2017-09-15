f3dc7946109f732a0813e32323425f1c1838ab68
Wilson Wang <wilsonw926@gmail.com>
2017-09-15 10:53:14 -0700

    finished hw1

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,14 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    sum = 0
+    a.map! { |x| x.to_i }
+    a.map! { |x| x + 2 }
+    a.keep_if { |x| x%2==0 }
+    a.uniq!
+    a.keep_if { |x| x<=10 }
+    a.each {|x| sum += x}
+    return sum
   end
 end
 
